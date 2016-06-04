5f977e24a3e060b30fbb5620e83f31f5032ef860
Alejandro Londono <alondono11@gmail.com>
2016-06-04 16:16:37 -0400

    adding UI

---
+++ openDoors/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+     
+.controller('splashSigninCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('marketplaceCtrl', function($scope) {
+
+})
+   
+.controller('bidStatusCtrl', function($scope) {
+
+})
+   
+.controller('placeBidCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+ 
---
+++ openDoors/www/js/routes.js
@@ -0,0 +1,59 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('splashSignin', {
+    url: '/page5',
+    templateUrl: 'templates/splashSignin.html',
+    controller: 'splashSigninCtrl'
+  })
+
+  .state('signup', {
+    url: '/page6',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('marketplace', {
+    url: '/page7',
+    templateUrl: 'templates/marketplace.html',
+    controller: 'marketplaceCtrl'
+  })
+
+  .state('bidStatus', {
+    url: '/page8',
+    templateUrl: 'templates/bidStatus.html',
+    controller: 'bidStatusCtrl'
+  })
+
+  .state('placeBid', {
+    url: '/page10',
+    templateUrl: 'templates/placeBid.html',
+    controller: 'placeBidCtrl'
+  })
+
+  .state('login', {
+    url: '/page11',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page7')
+
+  
+
+});
